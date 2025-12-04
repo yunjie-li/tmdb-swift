bb9d6a9865088a2db1c56528957e7fc7c5f35d38
yunjie.li <yl@m2.com>
2025-12-04 17:45:29 +0800

    修改tmdb mdb 域名

--- Sources/TMDB/TMDBClient.swift
+++ Sources/TMDB/TMDBClient.swift
@@ -1,4 +1,5 @@
 import Foundation
+import CryptoKit
 
 #if canImport(FoundationNetworking)
   import FoundationNetworking
@@ -26,7 +27,7 @@ public final class TMDBClient: Sendable {
     relativePath: String,
     queryItems: [URLQueryItem]? = nil
   ) throws -> URLRequest {
-    let baseURLString = "https://tmdb-proxy-tau-gules.vercel.app/3/"
+    let baseURLString = "https://tmdb.wwmm.date/3/"
     var components = URLComponents(string: baseURLString)!
     components.path.append(relativePath)
     components.queryItems = queryItems
@@ -34,11 +35,24 @@ public final class TMDBClient: Sendable {
       throw TMDBClientError.invalidURL(components)
     }
     var urlRequest = URLRequest(url: url)
+    let sign = generateSignature(appName: "Vidora")
     urlRequest.allHTTPHeaderFields = [
       "Accept": "application/json",
       "Authorization": "Bearer \(accessToken)",
+      "X-CLIENT-SIGN": sign.signature,
+      "X-CLIENT": "Vidora",
+      "X-CLIENT-TIMESTAMP": sign.timestamp
     ]
     return urlRequest
+  }
+  
+  func generateSignature(appName: String) -> (timestamp: String, signature: String) {
+      let timestamp = String(Int(Date().timeIntervalSince1970))
+      let raw = appName + timestamp + appName
+      let hash = SHA256.hash(data: raw.data(using: .utf8)!)
+      let signature = hash.compactMap { String(format: "%02x", $0) }.joined()
+      let short = String(signature.prefix(24))
+      return (timestamp, short)
   }
 
   public typealias PerformRequest = @Sendable (URLRequest) async throws -> Response
