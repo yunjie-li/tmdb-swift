4791ff68975aebf02969f62c74601bb312fdb673
yunjie.li <yl@m2.com>
2026-02-25 22:09:15 +0800

    add searchMulti

--- Sources/TMDB/Common/TMDBClient+Search.swift
+++ Sources/TMDB/Common/TMDBClient+Search.swift
@@ -84,4 +84,44 @@ extension TMDBClient {
     )
     return try await searchTVShow(parameters: parameters)
   }
+
+  public func searchMulti(
+    parameters: SearchParameters
+  ) async throws -> Page<MediaDetail> {
+    let queryItems = parameters.toQueryItems()
+
+    let urlRequest = try urlRequest(
+      relativePath: "search/multi",
+      queryItems: queryItems
+    )
+    let response = try await performRequest(urlRequest)
+
+    if response.statusCode == 200 {
+      let mediaPage = try decoder.decode(Page<Media>.self, from: response.data)
+      let mediaDetails = mediaPage.results.map { MediaDetail(from: $0) }
+      return Page<MediaDetail>(
+        page: mediaPage.page,
+        results: mediaDetails,
+        totalPages: mediaPage.totalPages,
+        totalResults: mediaPage.totalResults
+      )
+    } else {
+      throw try decoder.decode(TMDBError.self, from: response.data)
+    }
+  }
+
+  public func searchMulti(
+    query: String,
+    includeAdult: Bool? = nil,
+    language: String? = nil,
+    page: Int? = nil
+  ) async throws -> Page<MediaDetail> {
+    let parameters = SearchParameters(
+      query: query,
+      includeAdult: includeAdult,
+      language: language,
+      page: page
+    )
+    return try await searchMulti(parameters: parameters)
+  }
 }
